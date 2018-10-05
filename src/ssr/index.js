e724fa5cda7826d90ad0b28708d5b52b2a7e7ed9
Fi1osof <n.lanets@modxclub.ru>
2018-10-05 17:02:32 +0300

    1.3.3-beta

--- src/ssr/index.js
+++ src/ssr/index.js
@@ -44,7 +44,7 @@ require('@babel/register')({
   // },
 });
 
-['.css', '.less', '.sass', '.ttf', '.woff', '.woff2', '.svg'].forEach((ext) => require.extensions[ext] = () => { });
+['.css', '.less', '.sass', '.ttf', '.woff', '.woff2', '.svg', '.png'].forEach((ext) => require.extensions[ext] = () => { });
 // ['.css', '.less', '.sass', '.ttf', '.woff', '.woff2'].forEach((ext) => require.extensions[ext] = () => {});
 
 require('@babel/polyfill');
