122f21fccc7a89c949f6715ec2173d35052b88dd
Fi1osof <n.lanets@modxclub.ru>
2018-10-28 02:06:12 +0300

    2.0.3

--- src/server/index.mjs
+++ src/server/index.mjs
@@ -1,72 +1,21 @@
 
-
-// const CoreModule = require("./modules").default;
-// const coreModule = new CoreModule({
-// });
-// const resolvers = coreModule.getResolvers();
-
-// const imagesMiddleware = require("./middleware/ImageThumb");
-
-
-// switch (process.env.action) {
-
-//   case "build-schema":
-
-//     require("./schema").default(process.env.schemaType);
-
-//     break;
-
-//   case "start-server":
-
-//     const startServer = require("@prisma-cms/server").default;
-
-
-//     startServer({
-//       typeDefs: 'src/schema/generated/api.graphql',
-//       resolvers,
-//       imagesMiddleware,
-//     });
-
-//     break;
-
-// }
-
-
-
-
 import startServer from "@prisma-cms/server";
-
-// console.log("startServer", startServer);
-
-// const CoreModule = require("./modules").default;
-
 import CoreModule from "./modules";
-// console.log("CoreModule", CoreModule);
+import { ImagesMiddleware } from "@prisma-cms/upload-module";
+
 
 const coreModule = new CoreModule({
 });
 
 const resolvers = coreModule.getResolvers();
 
-
-import imagesMiddleware from "./middleware/ImageThumb";
-
-
-switch (process.env.action) {
-
-  case "start-server":
-
-    // const startServer = require("@prisma-cms/server").default;
+const imagesMiddleware = new ImagesMiddleware().processRequest;
 
 
-    startServer({
-      typeDefs: 'src/schema/generated/api.graphql',
-      resolvers,
-      imagesMiddleware,
-    });
-
-    break;
-
-}
+startServer({
+  typeDefs: 'src/schema/generated/api.graphql',
+  resolvers,
+  imagesMiddleware,
+});
 
 
