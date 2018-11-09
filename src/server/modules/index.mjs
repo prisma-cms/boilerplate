a8ce7a5a6d40bd29052710db4c46d4eb7766b349
Fi1osof <n.lanets@modxclub.ru>
2018-11-09 19:58:39 +0300

    Router module

--- src/server/modules/index.mjs
+++ src/server/modules/index.mjs
@@ -13,6 +13,7 @@ import LogModule from "@prisma-cms/log-module";
 import UserModule from "@prisma-cms/user-module";
 import MailModule from "@prisma-cms/mail-module";
 import UploadModule from "@prisma-cms/upload-module";
+import RouterModule from "@prisma-cms/router-module";
 
 
 import path from 'path';
@@ -36,6 +37,7 @@ class CoreModule extends CmsModule {
       LogModule,
       MailModule,
       UploadModule,
+      RouterModule,
     ]);
 
   }
