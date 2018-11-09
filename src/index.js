111bdc34af75bc62f8a3dccb162186f47b1a868e
Fi1osof <n.lanets@modxclub.ru>
2018-11-10 02:10:17 +0300

    me:fields

--- src/index.js
+++ src/index.js
@@ -9,8 +9,21 @@ import registerServiceWorker from './registerServiceWorker';
 
 import App from "./App";
 
+import {
+  UserNoNestingFragment,
+} from "./schema/generated/api.fragments";
+
 ReactDOM.render(<PrismaCms
   App={App}
+  apolloOptions={{
+    apiQuery: `{
+      user:me{
+        ...UserNoNesting
+      } 
+    }
+    ${UserNoNestingFragment}
+    `,
+  }}
 />, document.getElementById('root'));
 registerServiceWorker();
 
