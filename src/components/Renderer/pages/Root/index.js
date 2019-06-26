43511680439a66c12fa461a273d66e295dea3ada
Fi1osof <n.lanets@modxclub.ru>
2019-06-26 11:10:14 +0300

    3.2.1

--- src/components/Renderer/pages/Root/index.js
+++ src/components/Renderer/pages/Root/index.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import PrismaCmsComponent from "@prisma-cms/component";
 import RootConnector from '@prisma-cms/front-editor/lib/components/Root';
 import UserPage from './components/pages/Users/User';
-import PdfView from './components/PdfView';
+// import PdfView from './components/PdfView';
 import CreateUserPage from './components/pages/Users/User/Create';
 
 
@@ -22,7 +22,7 @@ class RootPage extends PrismaCmsComponent {
       CustomComponents={CustomComponents.concat([
         UserPage,
         CreateUserPage,
-        PdfView,
+        // PdfView,
       ])}
       {...other}
     />
